f00b2cf9c882262cbf5fa2467f60058bd1c488dd
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2022-08-22 22:54:17 +0300

    End of the day 6

--- client/my-app/src/components/Movie.js
+++ client/my-app/src/components/Movie.js
@@ -7,7 +7,6 @@ function Movies(props) {
       <div className="film-container">
         {props.movies.map((m) => (
           <div className="film-card">
-            <h1>{m.id}</h1>
             <img src={m.poster_path}></img>
             <h2 key={m.id}>{m.title}</h2>
           </div>
