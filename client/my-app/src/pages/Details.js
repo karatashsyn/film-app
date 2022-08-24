e3fbffb46464f4afef61e96697b2b2d2e4024bfb
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2022-08-24 05:43:09 +0300

    End of the day 7

--- client/my-app/src/pages/Details.js
+++ client/my-app/src/pages/Details.js
@@ -24,8 +24,53 @@ function Details() {
   console.log(currentMovie)
   return (
     <>
-      <h1>Heeeeyyyys</h1>
-      <h1>{currentMovie.title}</h1>
+      <div className="fake-details-body">
+        <img alt="bg" src={currentMovie.poster_path} className="blur-background"></img>
+        <div className="main-container">
+          <img alt="Movie Poster" src={currentMovie.poster_path} className="poster"></img>
+          <div className="info-container">
+            <div className="except-cast">
+              <div className="first-row">
+                <p className="title">{currentMovie.title}</p>
+                <div className="buttons">
+                  <div className="edit-btn">Edit</div>
+                  <div className="delete-btn"></div>
+                </div>
+              </div>
+              <div className="genres">Adventure, Horror</div>
+              <div className="description-block">
+                <p className="description-text">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, repellat
+                  facilis. Sapiente unde autem at, fuga modi qui fugiat ipsam laboriosam aspernatur
+                  eaque ullam. Rerum voluptatum error libero reprehenderit quibusdam? Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Repellendus, repellat facilis.
+                  Sapiente unde autem at, fuga modi qui fugiat ipsam laboriosam aspernatur eaque
+                  ullam. Rerum voluptatum error libero reprehenderit quibusdam?adipisicing elit.
+                  Repellendus, repellat facilis. Sapiente unde autem at, fuga modi qui fugiat ipsam
+                  laboriosam aspernatur eaque ullam. Rerum voluptatum error libero reprehenderit
+                  quibusdam?m laboriosam aspernatur eaque ullam. Rerum voluptatum error libero
+                  reprehenderit quibusdam?adipisicing elit. Repellendus, repellat facilis. Sapiente
+                  unde autem at, fuga modi qui fugiat ipsam laboriosam aspernatur eaque ullam. Rerum
+                  voluptatum error libero reprehenderit quibusdam?m laboriosam aspernatur eaque
+                  ullam. Rerum voluptatum error libero reprehenderit quibusdam?adipisicing elit.
+                  Repellendus, repellat facilis. Sapiente unde autem at, fuga
+                </p>
+              </div>
+            </div>
+            <div className="cast">
+              <div className="artist">
+                <div className="artist-photo"></div>
+                <p className="artist-name">Some Actor</p>
+              </div>
+
+              <div className="artist">
+                <div className="artist-photo"></div>
+                <p className="artist-name">Some Actor</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
