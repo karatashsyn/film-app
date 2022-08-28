abb08c4258811f3da69c85d1c9bdc5d970fdf01f
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2022-08-29 00:38:02 +0300

    Checkpoint

--- client/my-app/src/pages/Details.js
+++ client/my-app/src/pages/Details.js
@@ -297,33 +297,39 @@ function Details() {
                   </button>
                 </div>
               </div>
-              {searchedArtists.map((a) => (
-                <div>
-                  <div className="artist-container">
-                    <div
-                      onClick={() => {
-                        if (!selectedArtists.map((artist) => artist.id).includes(a.id)) {
-                          setSelectedArtists([...selectedArtists, a])
-                        }
-                        setArtistAdded(!artistAdded)
-                      }}
-                      className="artist-box"
-                      style={{
-                        pointerEvents: selectedArtists.map((artist) => artist.id).includes(a.id)
-                          ? 'none'
-                          : 'auto',
-                      }}
-                    >
-                      <h3>
-                        {selectedArtists.map((artist) => artist.id).includes(a.id)
-                          ? `${a.name} (Added)`
-                          : `${a.name}`}
-                      </h3>
-                      <img className="artist-box-image" src={a.profile_path}></img>
+              <>
+                {searchedArtists.length >= 1 ? (
+                  searchedArtists.map((a) => (
+                    <div>
+                      <div className="artist-container">
+                        <div
+                          onClick={() => {
+                            if (!selectedArtists.map((artist) => artist.id).includes(a.id)) {
+                              setSelectedArtists([...selectedArtists, a])
+                            }
+                            setArtistAdded(!artistAdded)
+                          }}
+                          className="artist-box"
+                          style={{
+                            pointerEvents: selectedArtists.map((artist) => artist.id).includes(a.id)
+                              ? 'none'
+                              : 'auto',
+                          }}
+                        >
+                          <h3>
+                            {selectedArtists.map((artist) => artist.id).includes(a.id)
+                              ? `✔️  ${a.name}  `
+                              : `${a.name}`}
+                          </h3>
+                          <img className="artist-box-image" src={a.profile_path}></img>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              ))}
+                  ))
+                ) : (
+                  <div className="no-result">No Result</div>
+                )}
+              </>
             </div>
           </div>
         </div>
